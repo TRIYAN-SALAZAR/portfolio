97bd1131d8783a2b758b6841c30568331a554283
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-09-18 02:13:53 +0000

    Change my info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jorge Ozuna",
+  title: "Web Backend Developer & Computer Engineering",
+  email: "jorgea.ozunasalazar@gmail.com.com",
+  gitHub: "TRIYAN-SALAZAR",
+  instagram: "triyan_salazar",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
